08bc8240c2f2cc0646bae4ebbb9a6ae36e1c02ca
golerichal <golerichal00@gmail.com>
2026-02-08 13:48:06 +0530

    Added Initial mini project files
    Include basic functionality and project setup

---
+++ firstproject.py
@@ -0,0 +1,31 @@
+print("Hello my friend")
+print("i am your mood advisor")
+print("How are you feeling today?")
+print("Options: happy, sad, angry, tired, stressed")
+
+mood = input("Enter your mood: ").lower()
+
+if mood == "happy":
+    print(" Awsomee!! 😊")
+    print("Advice: Enjoy this happiness, stay grateful, and keep doing what brings you peace. 🤩")
+
+elif mood == "sad":
+    print(" I'm sorry you're feeling sad 😕.")
+    print("Advice: Talk to someone you love ❤️.")
+
+elif mood == "angry":
+    print(" it's ok..it's natural.")
+    print("Advice: Take deep breaths and talk to yourself sometime.")
+
+elif mood == "stressed":
+    print("Relax you are human'Not Robot")
+    print("Advice: DO work that you love to do")
+
+elif mood == "tired":
+    print("After doing too much work it's natural😪")
+    print("Advice: Go and sleep")
+
+else:
+    print("I couldn't understand your mood.")
+    print("Advice: can you explain me little bit  clearly")
+ssssssssss
