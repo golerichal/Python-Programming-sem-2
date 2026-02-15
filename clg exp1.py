1296225d2b41c27395eadc4fc28698a58bfecf1d
golerichal <golerichal00@gmail.com>
2026-02-15 14:43:39 +0530

    Add files via upload

---
+++ clg exp1.py
@@ -0,0 +1,35 @@
+#Calculation
+
+#Addition
+print("Program for Addition")
+Num1=int(input("Enter 1st Number: "))
+Num2=int(input("Enter 2nd Number: "))
+sum=Num1+Num2
+print("Addition of Two NUmbers= ",sum)
+
+#Substraction
+print("Program for Substraction")
+Num1=int(input("Enter 1st number: "))
+Num2=int(input("Enter 2nd Number: "))
+sub=Num1-Num2
+print("Substraction of two numbers= ",sub)
+
+#Multiplication
+print("Program for Multiplication")
+Num1=int(input("enter 1st number: "))
+Num2=int(input("Enter 2nd number: "))
+Mult=Num1*Num2
+print("MUltiplication of two numbers: ",Mult)
+
+#Division
+print("Program for Division")
+Num1=int(input("Enter 1st number: "))
+Num2=int(input("Enter 2nd number: "))
+Div=Num1/Num2
+print("Division of two numbers: ",Div)
+
+#Module
+print("Program for Module")
+Num1=int(input("Enter 1st number: "))
+Num2=int(input("Enter 2nd Number: "))
+print("Modulus of two numbers= ",Num1%Num2)
